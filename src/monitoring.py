b7c2daf1158fa2af9d198828f3f0b46a296f2cb8
SmileMaskL <143575228+SmileMaskL@users.noreply.github.com>
2025-06-18 22:12:43 +0900

    Update monitoring.py

--- src/monitoring.py
+++ src/monitoring.py
@@ -9,3 +9,12 @@ def init_monitoring():
         profiles_sample_rate=1.0,
         environment="production" if os.getenv("K_SERVICE") else "github-actions"  # 환경 구분
     )
+
+def capture_error(context: dict):
+    import os
+    if os.getenv("K_SERVICE"):
+        sentry_sdk.set_tag("gcp_service", os.getenv("K_SERVICE"))
+    else:
+        sentry_sdk.set_tag("github_workflow", os.getenv("GITHUB_RUN_ID"))
+    
+    sentry_sdk.capture_message(f"Error in {context['module']}")
