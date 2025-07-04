2b2b82bbff092406b06f626c8e9f1708941a51b4
SmileMaskL <143575228+SmileMaskL@users.noreply.github.com>
2025-07-04 15:27:58 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,27 @@
 variable "project_id" {
-  description = "The GCP project ID to deploy resources into."
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  type        = string
+  default     = "youtube-shorts-automation"
+}
+
+variable "scheduler_name" {
+  description = "Cloud Scheduler job name"
+  type        = string
+  default     = "five-times-daily-youtube-shorts-upload"
+}
+
+variable "service_account_email" {
+  description = "Service account email for OIDC token"
   type        = string
 }
