915713d90aca1dfa61c82fd522ed70f541d87634
SmileMaskL <143575228+SmileMaskL@users.noreply.github.com>
2025-07-11 14:58:42 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import FastAPI, UploadFile, File, HTTPException, Form
 from fastapi.responses import JSONResponse
 
-from youtube_uploader import YouTubeUploader
+from .youtube_uploader import YouTubeUploader
 from video_engine import VideoEngine
 from secret_loader import secret_manager # SecretManager 인스턴스 임포트
 
